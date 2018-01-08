76cf0f3537ef78f4978f47ce647240d811a4afa2
Letarte <n0213502@libp45p-10645dy.lm.lmig.com>
2018-01-08 10:20:58 -0500

    Added jquery to bundle via app.js so all scripts can access it.

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,3 @@
-require(./main.js);
+// Adding jquery to the bundle. Adding the usual alias $ to the global window object so it is accessible by other scripts.
+window.$ = require('jquery');
+require('./main.js');
